5eeb0ac38e5eed74a84d9a5a1f967861644d7ff7
dongwenxiao <cs_victor@126.com>
2018-04-23 15:14:09 +0800

    add clean file code

---
+++ tools/clean.js
@@ -0,0 +1,15 @@
+const del = require('del')
+
+const todo = [
+    'public/**',
+    '!public', 
+    '!public/CNAME',
+    '!public/other/**'
+]
+
+// del.sync(todo)
+del(todo).then(paths => {
+    // console.log('Clean public folder generate files.')
+    // console.log('Clean public folder generate files :\n', paths.join('\n'))
+})
+
